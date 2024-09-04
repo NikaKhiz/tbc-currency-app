42eaf7a6cd0dcb3755a27a71cde831165eaff378
NikaKhiz <khizambarelinika123@gmail.com>
2024-09-04 17:17:12 +0400

    modifie app layout remove unnecessary lines of code

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ currencies_list = [*CURRENCIES.keys()]
 
 def main():
 
-    global conversion_rate_label, base_curr, conversion_curr, result_label, amount_field
+    global conversion_rate_label, base_curr, conversion_curr, result_label, amount_field, main_frame, root
 
     root = tk.Tk()
     window_width = 800
@@ -43,67 +43,98 @@ def main():
     root.geometry(f'{window_width}x{window_height}+{x_offset}+{y_offset}')
     root.title('Currency Converter')
     root.resizable(width=False, height=False)
-
     
+    main_frame = tk.Frame(root, bd=1, relief=tk.FLAT)
+    main_frame.grid(row=0, column=0, sticky='nswe')
+    main_frame.grid_rowconfigure(0, weight=1)
+    main_frame.grid_rowconfigure(1, weight=1)
+    main_frame.grid_rowconfigure(2, weight=1)
+    main_frame.grid_columnconfigure(0, weight=1)
+
     # currencies frame
-    currency_frame = tk.Frame(root, bd=1, relief=tk.SUNKEN)
-    currency_frame.grid(row=0, column=0, sticky='nsew', padx=10, pady=10)
+    curr_frame = tk.Frame(main_frame, bd=1, relief=tk.SUNKEN)
+    curr_frame.grid(row=0, column=0, sticky='nswe', padx=10, pady=10)
+    curr_frame.grid_rowconfigure(0, weight=1)
+    curr_frame.grid_columnconfigure(0, weight=1)
+    curr_frame.grid_columnconfigure(1, weight=1)
 
-    base_curr_label = tk.Label(currency_frame, text='From', font=('Arial', 14)) 
-    base_curr_label.grid(row=0, column=0, sticky='W', padx=10, pady=10)
-
-    base_curr = tk.StringVar(currency_frame)
+    # base currency 
+    base_curr_frame = tk.Frame(curr_frame)
+    base_curr_frame.grid(row=0, column=0, sticky='nswe', padx=10, pady=10)
+    base_curr_label = tk.Label(base_curr_frame, text='From:', font=('Arial', 14))
+    base_curr_label.grid(row=0, column=0, sticky='nswe')
+    base_curr = tk.StringVar(base_curr_frame)
     base_curr.set(currencies_list[0])
-    base_curr_menu = tk.OptionMenu(currency_frame, base_curr, *currencies_list, command=on_curr_change)
-    base_curr_menu.grid(row=0, column=1, sticky='ew', padx=10, pady=10)
+    base_curr_menu = tk.OptionMenu(base_curr_frame, base_curr, *currencies_list, command=on_curr_change)
+    base_curr_menu.grid(row=1, column=0, sticky='nswe')
 
-    conversion_curr_label = tk.Label(currency_frame, text='To', font=('Arial', 14))
-    conversion_curr_label.grid(row=1, column=0, sticky='W', padx=10, pady=10)
-
-    conversion_curr = tk.StringVar(currency_frame)
+    # conversion currency
+    conversion_curr_frame = tk.Frame(curr_frame)
+    conversion_curr_frame.grid(row=0, column=1, sticky='nswe', padx=10, pady=10)
+    conversion_curr_label = tk.Label(conversion_curr_frame, text='To:', font=('Arial', 14))
+    conversion_curr_label.grid(row=0, column=0, sticky='nswe')
+    conversion_curr = tk.StringVar(conversion_curr_frame)
     conversion_curr.set(currencies_list[1])
-    conversion_curr_menu = tk.OptionMenu(currency_frame, conversion_curr, *currencies_list, command=on_curr_change)
-    conversion_curr_menu.grid(row=1, column=1, sticky='ew', padx=10, pady=10)
+    conversion_curr_menu = tk.OptionMenu(conversion_curr_frame, conversion_curr, *currencies_list, command=on_curr_change)
+    conversion_curr_menu.grid(row=1, column=0, sticky='nswe')
 
-    conversion_rate_label = tk.Label(currency_frame, text=f'Rate : {convert(base_curr.get(), conversion_curr.get())}', font=('Arial', 14))
-    conversion_rate_label.grid(row=2, column=0, sticky='W', padx=10, pady=10)
-    
-
-    # actions frame
-    action_frame = tk.Frame(root, bd=1, relief=tk.SUNKEN)
-    action_frame.grid(row=1, column=0, sticky='nsew', padx=10, pady=10)
-
-    # amount for conversion
-    amount_frame = tk.Frame(action_frame, relief=tk.SUNKEN)
-    amount_frame.grid(row=0, column=0, sticky='ew', padx=10, pady=10)
+    # amount
+    amount_frame = tk.Frame(curr_frame)
+    amount_frame.grid(row=0, column=2, sticky='nswe', padx=10, pady=10)
     amount_label = tk.Label(amount_frame, text='Amount:', font=('Arial', 14))
-    amount_label.pack(side='left', padx=10, pady=10)
+    amount_label.grid(row=0, column=0, sticky='nswe')
     amount_field = tk.Entry(amount_frame, bg='lavender', highlightthickness=0)
-    amount_field.pack(side='left', padx=10, pady=10)
+    amount_field.grid(row=1, column=0, sticky='nswe')
 
-    # action buttons
-    btn_frame = tk.Frame(action_frame, relief=tk.SUNKEN)
-    btn_frame.grid(row=1, column=0, sticky='ew', padx=10, pady=10)
 
-    btn_convert = tk.Button(btn_frame, text='Convert', command=on_click_convert)
-    btn_convert.pack(side='left', padx=10, pady=10)
-    btn_clear = tk.Button(btn_frame, text='Clear', command=on_click_clear)
-    btn_clear.pack(side='left', padx=10, pady=10)
+    # exchange rate 
+    rate_frame = tk.Frame(curr_frame)
+    rate_frame.grid(row=1, column=0, columnspan=3, sticky='w', padx=10, pady=10)
+    conversion_rate_label = tk.Label (rate_frame, text=f'Rate: {convert(base_curr.get(),conversion_curr.get())}', font=('Arial', 14))
+    conversion_rate_label.pack(side='top', pady=5)
 
-    # result frame
-    result_frame = tk.Frame(root, bd=1, relief=tk.SUNKEN)
-    result_frame.grid(row=2, column=0, sticky='nsew', padx=10, pady=10)
+    # actions
+    action_frame = tk.Frame(main_frame, bd=1, relief=tk.SUNKEN)
+    action_frame.grid(row=1, column=0, sticky='nswe', padx=10, pady=10)
+    action_frame.grid_rowconfigure(0, weight=1)
+    action_frame.grid_rowconfigure(1, weight=1)
+    action_frame.grid_columnconfigure(0, weight=1)
+    action_frame.grid_columnconfigure(1, weight=1)
+    
+    btn_convert = tk.Button(action_frame, text='Convert', command=on_click_convert)
+    btn_convert.grid(row=0, column=0, padx=5, pady=5)
+    btn_clear = tk.Button(action_frame, text='Clear', command=on_click_clear)
+    btn_clear.grid(row=0, column=1, padx=5, pady=5)
 
-    result_label = tk.Label(result_frame, text='Result : ', font=('Arial', 14))
-    result_label.pack(side='left', padx=10, pady=10)
+    # result
+    result_frame = tk.Frame(main_frame, bd=1, relief=tk.SUNKEN)
+    result_frame.grid(row=2, column=0, sticky='nswe', padx=10, pady=10)
+    result_frame.grid_rowconfigure(0, weight=1)
 
-    root.grid_rowconfigure(0, weight=1)
-    root.grid_rowconfigure(1, weight=1)
-    root.grid_rowconfigure(2, weight=1)
-    root.grid_columnconfigure(0, weight=1)
+    result_label = tk.Label(result_frame, text='Result:', font=('Arial', 14))
+    result_label.pack(side='top', pady=5)
+
+
+    root.update_idletasks()
+    center_main_frame()  
 
     root.mainloop()
 
+
+# calculate the width and height of the main frame and according to that apply paddings for centering
+
+def center_main_frame():
+    main_frame.update_idletasks()  
+    main_frame_width = main_frame.winfo_width()
+    main_frame_height = main_frame.winfo_height()
+    
+    root_width = root.winfo_width()
+    root_height = root.winfo_height()
+    
+    padx = (root_width - main_frame_width) // 2
+    pady = (root_height - main_frame_height) // 2
+    
+    main_frame.grid_configure(padx=padx, pady=pady)
 
 # convert from one currency to another
 
